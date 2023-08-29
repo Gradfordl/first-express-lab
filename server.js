25ad102ff22efd274648c8a30968ca105f744f1c
gradfordl <laurengradford@gmail.com>
2023-08-29 11:16:18 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.get("/greeting/:name", (req, res) => {
 app.listen(3000, () => {
     console.log("Listening on port 3000, SUCCESS");
   });
+
+//===========================================================
+
+app.get("/tip/:total/:tip", (total, tipPercentage) => {
+    let tipCalc = parseInt(total.params.tip) / 100
+    tipPercentage.send(`Suggested tip amount is $${parseInt(total.params.total) * tipCalc}`)
+})
+
+//===========================================================
